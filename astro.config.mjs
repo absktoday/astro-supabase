580927876f476913cd81570bb35e9ee50f88d170
Abhishek Saral <abhisheksaral17@gmail.com>
2024-03-29 19:35:55 -0700

    Cloudflare instead of Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-supabase-auth.vercel.app",
   output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), solidJs()],
+  adapter: cloudflare(),
+  integrations: [tailwind(), solidJs()]
 });
